bf92ef3c9d08cf85adbc1440322e58c561a1a094
Abhishek Pathaj <pathajAbhishek@gmail.com>
2026-08-14 07:42:46 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Abhishek Pathak on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
